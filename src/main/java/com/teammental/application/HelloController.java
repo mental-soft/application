5bb1a318e5bd3e28025df44f0b389247b784c1a2
sefaaras <sefa666@hotmail.com>
2017-08-15 17:09:49 +0300

    AP-6 spring boot configuration

---
+++ src/main/java/com/teammental/application/HelloController.java
@@ -0,0 +1,15 @@
+package com.teammental.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+  @RequestMapping({"/", "/index"})
+  public String getIndex() {
+    return "index";
+  }
+
+}
+
