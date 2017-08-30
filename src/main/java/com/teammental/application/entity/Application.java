3a896ac7a955d390fdd37bcb8d9dd46148552b2b
sefaaras <sefa666@hotmail.com>
2017-08-30 17:25:41 +0300

    Create entities

---
+++ src/main/java/com/teammental/application/entity/Application.java
@@ -0,0 +1,65 @@
+package com.teammental.application.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ * Application tablosu modeli.
+ */
+
+@Entity
+@Table(name = "APPLICATION")
+public class Application {
+
+  @Id
+  @Column(name = "ID", columnDefinition = "NUMERIC")
+  private int id;
+
+  @Size(max = 100)
+  @Column(name = "KEY")
+  private String key;
+
+  @Size(max = 150)
+  @Column(name = "NAME")
+  private String name;
+
+  @Size(max = 300)
+  @Column(name = "DESCRIPTION")
+  private String description;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+}
---
+++ src/main/java/com/teammental/application/entity/Menu.java
@@ -0,0 +1,97 @@
+package com.teammental.application.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ * Menu tablosu modeli.
+ */
+
+@Entity
+@Table(name = "MENU")
+public class Menu {
+
+  @Id
+  @Column(name = "ID", columnDefinition = "NUMERIC")
+  private int id;
+
+  @Column(name = "PARENT_ID", columnDefinition = "NUMERIC")
+  private int parentId;
+
+  @Column(name = "APPLICATION_ID", columnDefinition = "NUMERIC")
+  private int applicationId;
+
+  @Size(max = 250)
+  @Column(name = "RELATIVE_URL")
+  private String relativeUrl;
+
+  @Size(max = 150)
+  @Column(name = "NAME")
+  private String name;
+
+  @Column(name = "ORDER", columnDefinition = "NUMERIC")
+  private int order;
+
+  @Size(max = 300)
+  @Column(name = "DESCRIPTION")
+  private String description;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getParentId() {
+    return parentId;
+  }
+
+  public void setParentId(int parentId) {
+    this.parentId = parentId;
+  }
+
+  public int getApplicationId() {
+    return applicationId;
+  }
+
+  public void setApplicationId(int applicationId) {
+    this.applicationId = applicationId;
+  }
+
+  public String getRelativeUrl() {
+    return relativeUrl;
+  }
+
+  public void setRelativeUrl(String relativeUrl) {
+    this.relativeUrl = relativeUrl;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getOrder() {
+    return order;
+  }
+
+  public void setOrder(int order) {
+    this.order = order;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
