c21fe4a5f4311a7f7984a8f3e84d8e5e27cf288d
sefaaras <sefa666@hotmail.com>
2017-10-10 15:53:01 +0300

    Added repository functions

--- src/main/java/com/teammental/application/jpa/ApplicationRepository.java
+++ src/main/java/com/teammental/application/jpa/ApplicationRepository.java
@@ -1,11 +1,17 @@
 package com.teammental.application.jpa;
 
 import com.teammental.application.entity.Application;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Application Repository Sınıfı.
  */
 
+@Repository
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
+
+  Application findByKey(String key);
+
 }
--- src/main/java/com/teammental/application/jpa/MenuRepository.java
+++ src/main/java/com/teammental/application/jpa/MenuRepository.java
@@ -1,11 +1,21 @@
 package com.teammental.application.jpa;
 
 import com.teammental.application.entity.Menu;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Menu Repository Sınıfı.
  */
 
+@Repository
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
+
+  List<Menu> findByApplicationId(Integer applicationId);
+
+  Menu findByRelativeUrl(String relativeUrl);
+
 }
