514752a14546a99ae61ae4a59f3210e28b1f8236
sefaaras <sefa666@hotmail.com>
2017-10-11 13:16:52 +0300

    Checkstyle error fixed

--- src/main/java/com/teammental/application/exception/CustomException.java
+++ src/main/java/com/teammental/application/exception/CustomException.java
@@ -6,22 +6,23 @@ package com.teammental.application.exception;
 
 public class CustomException extends RuntimeException {
 
-  public static String application_not_found = "Herhangi bir uygulama bulunamadı.";
-  public static String application_key_required = "Anahtar alanı zorunludur.";
-  public static String application_name_required = "İsim alanı zorunludur.";
-  public static String application_description_required = "Açıklama alanı zorunludur.";
-  public static String application_same_key = "Bu anahtar kelime ile bir uygulama zaten var.";
-  public static String application_save_error = "Kaydetme işlemi sırasında bir hata oluştu.";
-  public static String application_has_menu = "Bu uygulama silinemez. Uygulamaya bağlı işlemler vardır.";
+  public static final String application_not_found = "Herhangi bir uygulama bulunamadı.";
+  public static final String application_key_required = "Anahtar alanı zorunludur.";
+  public static final String application_name_required = "İsim alanı zorunludur.";
+  public static final String application_description_required = "Açıklama alanı zorunludur.";
+  public static final String application_same_key = "Bu anahtar kelime ile bir uygulama zaten var.";
+  public static final String application_save_error = "Kaydetme işlemi sırasında bir hata oluştu.";
+  public static final String application_has_menu = "Bu uygulama silinemez. "
+      + "Uygulamaya bağlı işlemler vardır.";
 
-  public static String menu_not_found = "Herhangi bir işlem bulunamadı.";
-  public static String menu_app_required = "Uygulama alanı zorunludur.";
-  public static String menu_url_required = "URL alanı zorunludur.";
-  public static String menu_name_required = "İsim alanı zorunludur.";
-  public static String menu_order_required = "Sıra alanı zorunludur.";
-  public static String menu_description_required = "Açıklama alanı zorunludur.";
-  public static String menu_same_url = "Bu link ile bir işlem zaten var.";
-  public static String menu_save_error = "Kaydetme işlemi sırasında bir hata oluştu.";
+  public static final String menu_not_found = "Herhangi bir işlem bulunamadı.";
+  public static final String menu_app_required = "Uygulama alanı zorunludur.";
+  public static final String menu_url_required = "URL alanı zorunludur.";
+  public static final String menu_name_required = "İsim alanı zorunludur.";
+  public static final String menu_order_required = "Sıra alanı zorunludur.";
+  public static final String menu_description_required = "Açıklama alanı zorunludur.";
+  public static final String menu_same_url = "Bu link ile bir işlem zaten var.";
+  public static final String menu_save_error = "Kaydetme işlemi sırasında bir hata oluştu.";
 
   private int code;
   private String label;
